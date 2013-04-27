bd58ab69ba56973b3e5262902072a448ab242168
root <root@newpouy-VirtualBox.(none)>
2013-04-27 19:42:31 +0900

    date remove

--- WebContent/application/views/main.php
+++ WebContent/application/views/main.php
@@ -12,8 +12,6 @@
 						WAS서버 : 톰캣7.0 <br/> 서버사이드언어: Java, JSP <br /> 클라이언트사이드 언어: html, css, Javascript,
 						<a href="http://jquery.com">jQuery</a><br /> <br /> 어리버리한
 						초보개발자입니다만, 관련 기술에 관한 문의는 언제나 환영합니다! <br />
-						지금 시각은
-						<?php echo date('G:i:s'); ?><br />
 					</p>
 					<div></div>
 					<p>최종 업데이트: 2013.04.16</p>
